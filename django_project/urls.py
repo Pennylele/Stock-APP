aee936e8453ca77d69532a183c5e773365fa168b
penny <fangle0121@gmail.com>
2020-09-05 23:37:10 -0700

    updated STATIC_ROOT

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', include('stock.urls'))
 ]
 
+# if settings.DEBUG:
+# 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
